81ce2e42c576172e38e74a80612d45b69bd45df0
lixiaoqity <59097971+lixiaoqity@users.noreply.github.com>
2020-09-24 10:37:44 -0400

    upload testLinks file

---
+++ README.md
@@ -0,0 +1,28 @@
+TestLinks
+
+testLinks is a command-line tool for finding and reporting dead links in a file.
+
+Programming language
+JavaScript
+
+Install
+
+Please download or clone the repository to your computer. Then change current directory to be the repository. 
+
+npm init     --- Initial your node.js enviroment.
+npm link     --- Create a symbolic link.
+npm install  --- Install the modules.
+
+Usage
+
+testLinks    --- Help. 
+testLinks v/version  --- Check the version of tool.
+testLinks fileName   --- Test the dead links in a file.
+
+Features
+The tool is used to find and test the status of Urls which start with http:// or https:// in a file.
+
+The green output and "good" mean the url which you test is safe and the status code is 200.
+The red output and "bad" mean the url which you test is not safe and the status code is 400 or 404.
+The gray output and "unknow" mean other status.
+
---
+++ testLinks.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+const process = require("process");
+const chalk = require('chalk');
+const fs = require('fs');
+const pjson = require('./package.json');
+
+const request = require('request');
+const regex = /(https?)(:\/\/)([-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]+)/gi;
+
+let fileName = process.argv.slice(2);
+
+if (fileName.length < 1) {
+    console.log('Help:');
+    console.log('Input "testLinks v/version" in command line --- Check the command line tool version and name!');
+    console.log('Input "testLinks [file name]" in command line --- finding and reporting dead links!');
+    process.exit(1);
+}
+else {
+    if (fileName[0] == "v" || fileName[0] == "version") {
+        console.log("name: " + pjson.name);
+        console.log("version: " + pjson.version);
+    }
+    else {
+        for (let i = 1; i <= fileName.length; i++) {
+            try {
+
+                let data = fs.readFileSync(fileName[i - 1], 'utf8');
+                let urls = data.match(regex);
+
+                if (urls) {
+                    for (let u of urls) {
+                        try {
+                            request({url: u, method: 'HEAD'},(error,response)=>{
+                                if(!error){
+                                    if (response.statusCode == 200) {
+                                        console.log(chalk.green(u + ' is good.'));
+                                    }
+                                    else if (response.statusCode == 400 | response.statusCode == 404) {
+                                        console.log(chalk.red(u + ' is bad.'));  
+                                    }
+                                    else {
+                                        console.log(chalk.gray(u + ' is unknow.'));
+                                    }
+                                }
+                                else{
+                                    console.log(chalk.gray(u + ' is unknow.'));
+                                }
+                            })
+                        }
+                        catch (error) {
+                            console.log(error);
+                        }
+                    }
+                }
+                else {
+                    console.log("No suitable http url.");
+                }
+            } catch (e) {
+                console.log('Error:', e.stack);
+            }
+        }
+    }
+}
+
+
+
+
+
+
