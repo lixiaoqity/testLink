8590afc0a853f4b56d428e131007a2843b9a0ca1
lixiaoqity <59097971+lixiaoqity@users.noreply.github.com>
2020-10-23 22:57:09 -0400

    Refactoring index.js file to improve code readable
     *Split index.js into index.js and read-file.js
    
     *modify version check
    
     *promote help output

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
 const pjson = require('./package.json');
-const testLink = require("./src/test-link");
-const testLinkColor = require('./src/test-link-color');
+const readFile = require('./src/read-file');
 const findIgnoreUrls = require('./src/find-ignore-urls');
 require('dotenv').config({ path: "./config/keys.env" });
-
-const regex = /(https?)(:\/\/)([-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]+)/gi;
 
 let fileName = process.argv.slice(2);
 let flag = false;
@@ -16,12 +12,14 @@ let flagError = false;
 
 if (fileName.length < 1) {
     console.log('Help:');
-    console.log('Input "testLinks v/version" in command line --- Check the command line tool version and name!');
+    console.log('Input "testLinks -v/--version" in command line --- Check the command line tool version and name!');
     console.log('Input "testLinks [file name]" in command line --- finding and reporting dead links!');
+    console.log('Input "testLinks -j/--json [file name]" in command line --- output Json data!');
+    console.log('Input "testLinks -i/--ignore [ignore file name] [file name]" in command line --- finding and reporting dead links excluding ignore urls!');
     process.exit(1);
 }
 else {
-    if (fileName.length == 1 && (fileName[0] == "v" || fileName[0] == "version")) {
+    if (fileName.length == 1 && (fileName[0] == "-v" || fileName[0] == "--version")) {
         console.log("name: testLinks");
         console.log("version: " + pjson.version);
     }
@@ -32,7 +30,7 @@ else {
             flag = true;
             fileName = fileName.slice(1);
         }
-        if ((fileName[0] == "-j" || fileName[0] == "-json" || fileName[0] == "\j") && fileName.length > 1) {
+        if ((fileName[0] == "-j" || fileName[0] == "--json" || fileName[0] == "\j") && fileName.length > 1) {
             flagj = true;
             fileName = fileName.slice(1);
         }
@@ -44,53 +42,11 @@ else {
                 badLines.map(s => console.log(s));
                 console.log();
                 flagError = true;
-            }
-            
+            }           
             fileName = fileName.slice(2);
         }
-
-        for (let i = 1; i <= fileName.length && flagError == false; i++) {
-            let data;
-
-            fs.readFile(fileName[i - 1], (err, response) => {
-                if (err) {
-                    console.log(`File {${fileName[i - 1]}} can not be found. Please try again!`);
-                }
-                else {
-                    data = response.toString();
-                    let urls = data.match(regex);
-                    if (urls) {
-                        for (let u of urls) {
-                            if (!u.match(ingoreUrlRegex)) {
-                                try {
-                                    if (process.env.CLICOLOR == 1) {
-                                        if (flag && !u.startsWith("https")) {
-                                            testLinkColor(u.replace(/^http/, "https"), flagj);
-                                        }
-                                        else {
-                                            testLinkColor(u, flagj);
-                                        }
-                                    }
-                                    if (process.env.CLICOLOR == 0) {
-                                        if (flag && !u.startsWith("https")) {
-                                            testLink(u.replace(/^http/, "https"), flagj);
-                                        }
-                                        else {
-                                            testLink(u, flagj);
-                                        }
-                                    }
-                                }
-                                catch (error) {
-                                    console.log(error);
-                                }
-                            }
-                        }
-                    }
-                    else {
-                        console.log(`No suitable http(s) url in file {${fileName[i - 1]}}.`);
-                    }
-                }
-            })
+        for (let i = 1; i <= fileName.length && flagError == false; i++) {            
+            readFile(fileName[i-1],ingoreUrlRegex,flag,flagj);
         }
     }
 }
---
+++ src/read-file.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const testLink = require("./test-link");
+const testLinkColor = require('./test-link-color');
+const regex = /(https?)(:\/\/)([-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]+)/gi;
+
+const readFile = (fileName,ignoreUrls,flag,flagj)=>{
+    let data;
+    fs.readFile(fileName, (err, response) => {
+        if (err) {
+            console.log(`File {${fileName}} can not be found. Please try again!`);
+        }
+        else {
+            data = response.toString();
+            let urls = data.match(regex);
+            if (urls) {
+                for (let u of urls) {
+                    if (!u.match(ignoreUrls)) {
+                        try {
+                            if (process.env.CLICOLOR == 1) {
+                                if (flag && !u.startsWith("https")) {
+                                    testLinkColor(u.replace(/^http/, "https"), flagj);
+                                }
+                                else {
+                                    testLinkColor(u, flagj);
+                                }
+                            }
+                            if (process.env.CLICOLOR == 0) {
+                                if (flag && !u.startsWith("https")) {
+                                    testLink(u.replace(/^http/, "https"), flagj);
+                                }
+                                else {
+                                    testLink(u, flagj);
+                                }
+                            }
+                        }
+                        catch (error) {
+                            console.log(error);
+                        }
+                    }
+                }
+            }
+            else {
+                console.log(`No suitable http(s) url in file {${fileName[i - 1]}}.`);
+            }
+        }
+    })
+}
+
+module.exports=readFile;
