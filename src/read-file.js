5f1fa0c268f36ee8eac58901c37d3b71c74d7710
lixiaoqity <59097971+lixiaoqity@users.noreply.github.com>
2020-11-26 16:19:54 -0500

    modify unit test and create a pull request to test CI

--- src/read-file.js
+++ src/read-file.js
@@ -1,44 +1,44 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const testLink = require('./test-link');
 const testLinkColor = require('./test-link-color');
 const regex = /(https?)(:\/\/)([-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]+)/gi;
 
-const readFile = (fileName, ignoreUrls, flag, flagj) => {
+const readFile = async (fileName, ignoreUrls, flag, flagj) => {
 	let data;
-	fs.readFile(fileName, (err, response) => {
-		if (err) {
-			console.log(`File {${fileName}} can not be found. Please try again!`);
-		} else {
-			data = response.toString();
-			let urls = data.match(regex);
-			if (urls) {
-				for (let u of urls) {
-					if (!u.match(ignoreUrls)) {
-						try {
-							if (process.env.CLICOLOR == 1) {
-								if (flag && !u.startsWith('https')) {
-									testLinkColor(u.replace(/^http/, 'https'), flagj);
-								} else {
-									testLinkColor(u, flagj);
-								}
-							}
-							if (process.env.CLICOLOR == 0) {
-								if (flag && !u.startsWith('https')) {
-									testLink(u.replace(/^http/, 'https'), flagj);
-								} else {
-									testLink(u, flagj);
-								}
-							}
-						} catch (error) {
-							console.log(error);
+	try {
+		data = await fs.readFile(fileName, 'utf8');
+	} catch (error) {
+		console.log(`File {${fileName}} can not be found. Please try again!`);
+	}
+
+	let urls = data.match(regex);
+	if (urls) {
+		for (let u of urls) {
+			if (!u.match(ignoreUrls)) {
+				try {
+					if (process.env.CLICOLOR == 1) {
+						if (flag && !u.startsWith('https')) {
+							testLinkColor(u.replace(/^http/, 'https'), flagj);
+						} else {
+							testLinkColor(u, flagj);
 						}
 					}
+					if (process.env.CLICOLOR == 0) {
+						if (flag && !u.startsWith('https')) {
+							testLink(u.replace(/^http/, 'https'), flagj);
+						} else {
+							testLink(u, flagj);
+						}
+					}
+				} catch (error) {
+					console.log(error);
 				}
-			} else {
-				console.log(`No suitable http(s) url in file {${fileName}}.`);
 			}
 		}
-	});
+	} else {
+		console.log(`No suitable http(s) url in file {${fileName}}.`);
+	}
+	return data;
 };
 
 module.exports = readFile;
--- src/test-link-color.js
+++ src/test-link-color.js
@@ -1,8 +1,10 @@
-const chalk = require("chalk");
-const request = require("request");
+const chalk = require('chalk');
+const request = require('request');
 
 const testLinkColor = (u, j) => {
-	request({ url: u, method: "HEAD" }, (error, response) => {
+	let code;
+	request({ url: u, method: 'HEAD' }, (error, response) => {
+		code = response.statusCode;
 		if (j == true) {
 			let temp = {
 				url: u,
@@ -12,23 +14,25 @@ const testLinkColor = (u, j) => {
 		} else {
 			if (!error) {
 				if (response.statusCode == 200) {
-					console.log(chalk.green(u + " is good."));
+					console.log(chalk.green(u + ' is good.'));
 				} else if (response.statusCode == 400 || response.statusCode == 404) {
-					console.log(chalk.red(u + " is bad."));
+					console.log(chalk.red(u + ' is bad.'));
 				} else if (
 					response.statusCode == 301 ||
 					response.statusCode == 307 ||
 					response.statusCode == 308
 				) {
-					console.log(chalk.yellow(u + " is redirect."));
+					console.log(chalk.yellow(u + ' is redirect.'));
 				} else {
-					console.log(chalk.gray(u + " is unknow."));
+					console.log(chalk.gray(u + ' is unknow.'));
 				}
 			} else {
-				console.log(chalk.gray(u + " is unknow."));
+				console.log(chalk.gray(u + ' is unknow.'));
 			}
 		}
-	});
+	}).setMaxListeners(50);
+	console.log(code);
+	return code;
 };
 
 module.exports = testLinkColor;
