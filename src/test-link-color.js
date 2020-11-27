d9425b06b911c4af9532e48bdba7114f34402709
lixiaoqity <59097971+lixiaoqity@users.noreply.github.com>
2020-11-26 22:57:28 -0500

    fix small bugs

--- src/test-link-color.js
+++ src/test-link-color.js
@@ -2,37 +2,36 @@ const chalk = require('chalk');
 const request = require('request');
 
 const testLinkColor = (u, j) => {
-	let code;
-	request({ url: u, method: 'HEAD' }, (error, response) => {
-		code = response.statusCode;
-		if (j == true) {
-			let temp = {
-				url: u,
-				status: response.statusCode,
-			};
-			console.log(JSON.stringify(temp));
-		} else {
-			if (!error) {
-				if (response.statusCode == 200) {
-					console.log(chalk.green(u + ' is good.'));
-				} else if (response.statusCode == 400 || response.statusCode == 404) {
-					console.log(chalk.red(u + ' is bad.'));
-				} else if (
-					response.statusCode == 301 ||
-					response.statusCode == 307 ||
-					response.statusCode == 308
-				) {
-					console.log(chalk.yellow(u + ' is redirect.'));
+	return new Promise((resolve) => {
+		request({ url: u, method: 'HEAD' }, (error, response) => {
+			if (j == true) {
+				let temp = {
+					url: u,
+					status: response.statusCode,
+				};
+				console.log(JSON.stringify(temp));
+			} else {
+				if (!error) {
+					if (response.statusCode == 200) {
+						console.log(chalk.green(u + ' is good.'));
+					} else if (response.statusCode == 400 || response.statusCode == 404) {
+						console.log(chalk.red(u + ' is bad.'));
+					} else if (
+						response.statusCode == 301 ||
+						response.statusCode == 307 ||
+						response.statusCode == 308
+					) {
+						console.log(chalk.yellow(u + ' is redirect.'));
+					} else {
+						console.log(chalk.gray(u + ' is unknow.'));
+					}
 				} else {
 					console.log(chalk.gray(u + ' is unknow.'));
 				}
-			} else {
-				console.log(chalk.gray(u + ' is unknow.'));
 			}
-		}
-	}).setMaxListeners(50);
-	console.log(code);
-	return code;
+			resolve();
+		}).setMaxListeners(50);
+	});
 };
 
 module.exports = testLinkColor;
